9802144929bff7e89c34658f46a6ed398c6f335a
37www <50452088+37www@users.noreply.github.com>
2019-05-16 15:41:41 +0800

    Add files via upload

---
+++ SanJing.SMS/SanJing.SMS/Aliyun.cs
@@ -0,0 +1,57 @@
+﻿using Aliyun.Acs.Core;
+using Aliyun.Acs.Core.Profile;
+using Aliyun.Acs.Dysmsapi.Model.V20170525;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SanJing.SMS
+{
+    /// <summary>
+    /// 阿里云
+    /// </summary>
+    public class Aliyun
+    {
+        const string REGIONIDFORPOP = "cn-hangzhou";
+        const string PRODUCT = "Dysmsapi";
+        const string DOMAIN = "dysmsapi.aliyuncs.com";
+        const string OK = "OK";
+        /// <summary>
+        /// 群发短信
+        /// </summary>
+        /// <param name="templateCode">模板ID|管理控制台中配置的审核通过的短信模板的模板CODE（状态必须是验证通过）</param>
+        /// <param name="templateParam">模板中的变量</param>
+        /// <param name="phoneNumbers">手机号</param>
+        /// <param name="signName">签名|管理控制台中配置的短信签名（状态必须是验证通过）</param>
+        /// <param name="accessId">APPID</param>
+        /// <param name="accessSecret">APPKEY</param>
+        public static void Send(string templateCode, Dictionary<string, string> templateParam, string[] phoneNumbers, string signName, string accessId, string accessSecret)
+        {
+            IClientProfile profile = DefaultProfile.GetProfile(REGIONIDFORPOP, accessId, accessSecret);
+            DefaultProfile.AddEndpoint(REGIONIDFORPOP, REGIONIDFORPOP, PRODUCT, DOMAIN);
+            IAcsClient acsClient = new DefaultAcsClient(profile);
+            SendSmsRequest request = new SendSmsRequest();
+            request.PhoneNumbers = string.Join(",", phoneNumbers);
+            request.SignName = signName;
+            request.TemplateCode = templateCode;
+            request.TemplateParam = new System.Web.Script.Serialization.JavaScriptSerializer().Serialize(templateParam);
+            SendSmsResponse sendSmsResponse = acsClient.GetAcsResponse(request);
+            if (sendSmsResponse.Code != OK)
+                throw new Exception(new System.Web.Script.Serialization.JavaScriptSerializer().Serialize(sendSmsResponse));
+        }
+        /// <summary>
+        /// 单发短信
+        /// </summary>
+        /// <param name="templateCode">模板ID|管理控制台中配置的审核通过的短信模板的模板CODE（状态必须是验证通过）</param>
+        /// <param name="templateParam">模板中的变量</param>
+        /// <param name="phoneNumber">手机号</param>
+        /// <param name="signName">签名|管理控制台中配置的短信签名（状态必须是验证通过）</param>
+        /// <param name="accessId">APPID</param>
+        /// <param name="accessSecret">APPKEY</param>
+        public static void Send(string templateCode, Dictionary<string, string> templateParam, string phoneNumber, string signName, string accessId, string accessSecret)
+        {
+            Send(templateCode, templateParam, new[] { phoneNumber }, signName, accessId, accessSecret);
+        }
+    }
+}
---
+++ SanJing.SMS/SanJing.SMS/Tencent.cs
@@ -0,0 +1,62 @@
+﻿using qcloudsms_csharp;
+using qcloudsms_csharp.httpclient;
+using qcloudsms_csharp.json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SanJing.SMS
+{
+    /// <summary>
+    /// 腾讯云
+    /// </summary>
+    public class Tencent
+    {
+        /// <summary>
+        /// 指定模板 ID 单发短信(86)(默认签名)
+        /// </summary>
+        /// <param name="phoneNumber">手机号</param>
+        /// <param name="values">替换值|按顺序替换</param>
+        /// <param name="templateId">模板Id</param>
+        /// <param name="appid">APPID</param>
+        /// <param name="appkey">APPKEY</param>
+        public static void Send(int templateId, string[] values, string phoneNumber, int appid, string appkey)
+        {
+            SmsSingleSender ssender = new SmsSingleSender(appid, appkey);
+            var result = ssender.sendWithParam("86", phoneNumber, templateId, values, "", "", "");
+            if (result.result != 0)
+                throw new Exception(new System.Web.Script.Serialization.JavaScriptSerializer().Serialize(result));
+        }
+        /// <summary>
+        /// 指定模板 ID 单发短信(86)(默认签名)
+        /// </summary>
+        /// <param name="templateId">模板Id</param>
+        /// <param name="values">替换值|按顺序替换</param>
+        /// <param name="phoneNumbers">手机号</param>
+        /// <param name="appid">APPID</param>
+        /// <param name="appkey">APPKEY</param>
+        public static void Send(int templateId, string[] values, string[] phoneNumbers, int appid, string appkey)
+        {
+            SmsMultiSender msender = new SmsMultiSender(appid, appkey);
+            var result = msender.sendWithParam("86", phoneNumbers, templateId, values, "", "", "");
+            if (result.result != 0)
+                throw new Exception(new System.Web.Script.Serialization.JavaScriptSerializer().Serialize(result));
+        }
+        /// <summary>
+        /// 发送语音通知
+        /// 数字默认按照个十百千万进行播报
+        /// </summary>
+        /// <param name="voiceContent">语音文本|示例：您的语音验证码是5,6,7,8</param>
+        /// <param name="phoneNumber">手机号</param>
+        /// <param name="appid">APPID</param>
+        /// <param name="appkey">APPKEY</param>
+        public static void SendVoice(string voiceContent, string phoneNumber, int appid, string appkey)
+        {
+            SmsVoicePromptSender vspsender = new SmsVoicePromptSender(appid, appkey);
+            var result = vspsender.send("86", phoneNumber, 2, voiceContent, 2, "");
+            if (result.result != 0)
+                throw new Exception(new System.Web.Script.Serialization.JavaScriptSerializer().Serialize(result));
+        }
+    }
+}
