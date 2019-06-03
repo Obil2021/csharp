c306bdf4c6ea3aa476fc99368c84e2b1dee7ff63
37www <50452088+37www@users.noreply.github.com>
2019-06-03 12:13:45 +0800

    Add files via upload

---
+++ WebApiException/TokenException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SanJing.WebApi
+{
+    /// <summary>
+    /// 身份认证异常（失败）
+    /// </summary>
+    public class TokenException : Exception
+    {
+        /// <summary>
+        /// 实例化
+        /// </summary>
+        public TokenException() : base("凭据无效") { }
+    }
+}
