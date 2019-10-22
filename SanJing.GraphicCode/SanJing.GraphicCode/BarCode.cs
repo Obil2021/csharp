f93f3cb9daee0ff09a3f37227b45e3c43cdc2af9
home@inmacro.cn <home@inmacro.cn>
2019-10-22 21:14:03 +0800

    SanJing.GraphicCode
    SanJing.GraphicCode

---
+++ SanJing.GraphicCode/SanJing.GraphicCode/BarCode.cs
@@ -0,0 +1,62 @@
+﻿using BarcodeLib;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+
+namespace SanJing.GraphicCode
+{
+    public class BarCode
+    {
+        /// <summary>
+        /// 生成一维码（CODE128）图片（PNG）
+        /// </summary>
+        /// <param name="fileName">存储图片文件的完整路径</param>
+        /// <param name="codeNumber">条码内容</param>
+        /// <param name="includeLabel">条码上是否显示条码内容（默认显示）</param>
+        /// <param name="width">宽度（默认250）</param>
+        /// <param name="height">高度（默认100）</param>
+        public static void SaveAsCODE128(string fileName, string codeNumber, bool includeLabel = true, int width = 250, int height = 100)
+        {
+            using (var barcode = new Barcode()
+            {
+                IncludeLabel = includeLabel,
+                Alignment = AlignmentPositions.CENTER,
+                Width = width,
+                Height = height,
+                RotateFlipType = RotateFlipType.RotateNoneFlipNone,
+                BackColor = Color.White,
+                ForeColor = Color.Black,
+            })
+            {
+                barcode.Encode(TYPE.CODE128, codeNumber).Save(fileName, ImageFormat.Png);
+            }
+        }
+        /// <summary>
+        /// 生成一维码（CODE39）图片（PNG）
+        /// </summary>
+        /// <param name="fileName">存储图片文件的完整路径</param>
+        /// <param name="codeNumber">条码内容</param>
+        /// <param name="includeLabel">条码上是否显示条码内容（默认显示）</param>
+        /// <param name="width">宽度（默认250）</param>
+        /// <param name="height">高度（默认100）</param>
+        public static void SaveAsCODE39(string fileName, string codeNumber, bool includeLabel = true, int width = 250, int height = 100)
+        {
+            using (var barcode = new Barcode()
+            {
+                IncludeLabel = includeLabel,
+                Alignment = AlignmentPositions.CENTER,
+                Width = width,
+                Height = height,
+                RotateFlipType = RotateFlipType.RotateNoneFlipNone,
+                BackColor = Color.White,
+                ForeColor = Color.Black,
+            })
+            {
+                barcode.Encode(TYPE.CODE39, codeNumber).Save(fileName, ImageFormat.Png);
+            }
+        }
+    }
+}
---
+++ SanJing.GraphicCode/SanJing.GraphicCode/QRCode.cs
@@ -0,0 +1,31 @@
+﻿using Gma.QrCodeNet.Encoding;
+using Gma.QrCodeNet.Encoding.Windows.Controls;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+
+namespace SanJing.GraphicCode
+{
+    public class QRCode
+    {
+        /// <summary>
+        /// 生成二维码图片（PNG）
+        /// </summary>
+        /// <param name="fileName">存储图片文件的完整路径</param>
+        /// <param name="codeContent">条码内容</param>
+        /// <param name="moduleSize">1=33*33;2=66*66;3=99*99;4=132*132;5=165*165(默认2)</param>
+        public static void SaveAs(string fileName, string codeContent, int moduleSize = 2)
+        {
+            QrEncoder qrEncoder = new QrEncoder(ErrorCorrectionLevel.H);
+
+            QrCode qrCode = new QrCode();
+            qrEncoder.TryEncode(codeContent, out qrCode);
+
+            Renderer renderer = new Renderer(5, Brushes.Black, Brushes.White);
+            renderer.CreateImageFile(qrCode.Matrix, fileName, ImageFormat.Png);
+        }
+    }
+}
