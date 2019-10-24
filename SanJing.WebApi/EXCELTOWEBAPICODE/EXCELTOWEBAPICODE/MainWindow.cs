e5d066e97071fa7b13c5781f74e5d75bd11a5d14
37www <50452088+37www@users.noreply.github.com>
2019-10-24 16:55:53 +0800

    MSDBTOEXCEL
    MSDBTOEXCEL

---
+++ SanJing.WebApi/EXCELTOWEBAPICODE/EXCELTOWEBAPICODE/MainWindow.cs
@@ -0,0 +1,263 @@
+﻿using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EXCELTOWEBAPICODE
+{
+    public partial class MainWindow : MaterialSkin.Controls.MaterialForm
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EXCEL_TEXT_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            //dialog.Multiselect = true;//该值确定是否可以选择多个文件
+            // dialog.Title = "请选择文件夹";
+            dialog.Filter = "EXCEL2007文件(*.XLSX)|*.xlsx";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                if (string.IsNullOrEmpty(dialog.FileName))
+                {
+                    return;
+                }
+                EXCEL_TEXT.Text = dialog.FileName;
+            }
+        }
+
+        private void CS_TEXT_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                if (string.IsNullOrEmpty(dialog.SelectedPath))
+                {
+                    return;
+                }
+                CS_TEXT.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void EXPORT_BUTTON_Click(object sender, EventArgs e)
+        {
+            ERROR_LABLE.Text = string.Empty;
+
+            if (string.IsNullOrWhiteSpace(EXCEL_TEXT.Text))
+            {
+                ERROR_LABLE.Text = "EXCELFULLFILENAME IS NULL";
+                return;
+            }
+            if (!File.Exists(EXCEL_TEXT.Text.Trim()))
+            {
+                ERROR_LABLE.Text = "EXCELFULLFILENAME IS NOT EXIST";
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(CS_TEXT.Text))
+            {
+                ERROR_LABLE.Text = "WEBAPICSSAVEPATH IS NULL";
+                return;
+            }
+            if (!Directory.Exists(CS_TEXT.Text.Trim()))
+            {
+                ERROR_LABLE.Text = "WEBAPICSSAVEPATH IS NOT EXIST";
+                return;
+            }
+            try
+            {
+                var excel = SanJing.Excel.Import.ReadAs2007(EXCEL_TEXT.Text.Trim(), 9).ToArray();
+                if (excel.Length == 0)
+                {
+                    ERROR_LABLE.Text = $"EXCEL CONTENT IS NULL";
+                    return;
+                }
+                if (excel[0].Length != 10)
+                {
+                    ERROR_LABLE.Text = $"EXCEL FORMAT IS ERROR";
+                    return;
+                }
+                var namesapce = string.Empty;
+                var apiname = string.Empty;
+                var apidesc = string.Empty;
+                var fullexcel = new List<string[]>();
+                for (int i = 1; i < excel.Length; i++)
+                {
+                    var line = excel[i];
+                    if (i == 1)
+                    {
+                        if (string.IsNullOrWhiteSpace(line[0]))
+                        {
+                            ERROR_LABLE.Text = $"{excel[0][0]}:必填，位于{i}行";
+                            return;
+                        }
+                        if (string.IsNullOrWhiteSpace(line[1]))
+                        {
+                            ERROR_LABLE.Text = $"{excel[0][1]}:必填，位于{i}行";
+                            return;
+                        }
+                        namesapce = line[0];
+                        apiname = line[1];
+                    }
+                    else
+                    {
+                        if (string.IsNullOrWhiteSpace(line[0]))
+                        {
+                            line[0] = namesapce;
+                        }
+                        else
+                        {
+                            namesapce = line[0];
+                        }
+                        if (string.IsNullOrWhiteSpace(line[1]))
+                        {
+                            line[1] = apiname;
+                        }
+                        else
+                        {
+                            apiname = line[1];
+                            apidesc = string.Empty;
+                        }
+                    }
+                    if (string.IsNullOrWhiteSpace(apidesc))
+                    {
+                        if (string.IsNullOrWhiteSpace(line[2]))
+                        {
+                            apidesc = apiname;
+                            line[2] = apiname;
+                        }
+                        else
+                        {
+                            apidesc = line[2];
+                        }
+                    }
+                    else
+                    {
+                        if (string.IsNullOrWhiteSpace(line[2]))
+                        {
+                            line[2] = apidesc;
+                        }
+                        else
+                        {
+                            apidesc = line[2];
+                        }
+                    }
+                    fullexcel.Add(line);
+                }
+                var namespaces = fullexcel.GroupBy(q => q[0]).ToArray();
+                foreach (var item in namespaces)
+                {
+                    var apis = item.GroupBy(q => q[1]).ToArray();
+                    foreach (var item1 in apis)
+                    {
+                        var request = item1.Where(q => !string.IsNullOrWhiteSpace(q[3])).ToArray();
+                        var response = item1.Where(q => !string.IsNullOrWhiteSpace(q[8])).ToArray();
+                        var code = Properties.Resources.String1.Replace("MvcApplication2", item.Key);
+                        code = code.Replace("UserInfo", item1.Key);
+                        code = code.Replace("userInfo", item1.Key.Substring(0, 1).ToLower() + item1.Key.Substring(1));
+                        code = code.Replace("用户信息", item1.First()[2]);
+
+                        foreach (var item2 in request)
+                        {
+                            var propmodel = @"/// <summary>
+            /// @密码
+            /// </summary>
+            @[Required]
+            @[StringLength(128, MinimumLength = 32)]
+            public @string @Password { get; set; }@Value
+            @RequestClass";
+                            propmodel = propmodel.Replace("@密码", item2[7]);
+                            propmodel = propmodel.Replace("@[Required]", string.IsNullOrWhiteSpace(item2[5]) ? string.Empty : $"[{item2[5]}]");
+                            propmodel = propmodel.Replace("@[StringLength(128, MinimumLength = 32)]", string.IsNullOrWhiteSpace(item2[6]) ? string.Empty : $"[{item2[6]}]");
+                            propmodel = propmodel.Replace("@string", item2[4]);
+                            propmodel = propmodel.Replace("@Password", item2[3]);
+                            propmodel = propmodel.Replace("@Value", item2[4].ToLower() == "string" ? " = string.Empty;" : string.Empty);
+                            code = code.Replace("@RequestClass", propmodel);
+                        }
+                        code = code.Replace("@RequestClass", string.Empty);
+
+                        foreach (var item2 in response.GroupBy(q => q[8].Split('.')[0]))
+                        {
+                            if (item2.Count() == 1)
+                            {
+                                var propmodel = @"/// <summary>
+            /// @密码
+            /// </summary>
+            public string @Password { get; set; } = string.Empty;
+            @ResponseClass";
+                                propmodel = propmodel.Replace("@密码", item2.First()[9]);
+                                propmodel = propmodel.Replace("@Password", item2.First()[8]);
+                                code = code.Replace("@ResponseClass", propmodel);
+                            }
+                            else
+                            {
+                                try
+                                {
+                                    var cla = item2.Single(q => !q[8].Contains("."));
+                                    var propmodel = @"/// <summary>
+            /// @密码
+            /// </summary>
+            public @string[] @Password { get; set; } = new @string[0];
+            @ResponseClass";
+                                    propmodel = propmodel.Replace("@密码", cla[9]);
+                                    propmodel = propmodel.Replace("@string", cla[8] + "Item");
+                                    propmodel = propmodel.Replace("@Password", cla[8]);
+                                    code = code.Replace("@ResponseClass", propmodel);
+
+                                    propmodel = @"/// <summary>
+            /// @密码
+            /// </summary>
+            public class @Password
+            {
+                @ResponseClassItem
+            }
+            @ResponseClass";
+                                    propmodel = propmodel.Replace("@密码", cla[9]);
+                                    propmodel = propmodel.Replace("@Password", cla[8] + "Item");
+                                    code = code.Replace("@ResponseClass", propmodel);
+                                    foreach (var item3 in item2)
+                                    {
+                                        if (item3 == cla)
+                                            continue;
+                                        propmodel = @"/// <summary>
+                /// @密码
+                /// </summary>
+                public string @Password { get; set; } = string.Empty;
+                @ResponseClassItem";
+                                        propmodel = propmodel.Replace("@密码", item3[9]);
+                                        propmodel = propmodel.Replace("@Password", item3[8].Replace(cla[8] + ".", string.Empty));
+                                        code = code.Replace("@ResponseClassItem", propmodel);
+                                    }
+                                    code = code.Replace("@ResponseClassItem", string.Empty);
+                                }
+                                catch(Exception ex)
+                                {
+                                    ERROR_LABLE.Text = $"{item2.Key} FORMAT IS ERROR";
+                                    return;
+                                }
+                            }
+                        }
+                        code = code.Replace("@ResponseClass", string.Empty);
+                        File.WriteAllText($@"{CS_TEXT.Text}\{item1.Key}Controller.cs", code, Encoding.UTF8);
+                    }
+                }
+                ERROR_LABLE.Text = "SUCCESS";
+            }
+            catch (Exception ex) { ERROR_LABLE.Text = ex.Message; }
+        }
+    }
+}
---
+++ SanJing.WebApi/MSDBTOEXCEL/MSDBTOEXCEL/MainWindow.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace MSDBTOEXCEL
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DB_LABLE = new MaterialSkin.Controls.MaterialLabel();
+            this.EXCEL_TEXT = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.EXCEL_LABLE = new MaterialSkin.Controls.MaterialLabel();
+            this.EXPORT_BUTTON = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.ERROR_LABLE = new System.Windows.Forms.Label();
+            this.DB_TEXT = new System.Windows.Forms.ComboBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // DB_LABLE
+            // 
+            this.DB_LABLE.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.DB_LABLE.AutoSize = true;
+            this.DB_LABLE.Depth = 0;
+            this.DB_LABLE.Font = new System.Drawing.Font("Roboto", 11F);
+            this.DB_LABLE.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.DB_LABLE.Location = new System.Drawing.Point(64, 95);
+            this.DB_LABLE.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.DB_LABLE.MouseState = MaterialSkin.MouseState.HOVER;
+            this.DB_LABLE.Name = "DB_LABLE";
+            this.DB_LABLE.Size = new System.Drawing.Size(183, 19);
+            this.DB_LABLE.TabIndex = 0;
+            this.DB_LABLE.Text = "DB CONNECTIONSTRING:";
+            // 
+            // EXCEL_TEXT
+            // 
+            this.EXCEL_TEXT.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.EXCEL_TEXT.Depth = 0;
+            this.EXCEL_TEXT.Hint = "";
+            this.EXCEL_TEXT.Location = new System.Drawing.Point(68, 177);
+            this.EXCEL_TEXT.Margin = new System.Windows.Forms.Padding(4);
+            this.EXCEL_TEXT.MouseState = MaterialSkin.MouseState.HOVER;
+            this.EXCEL_TEXT.Name = "EXCEL_TEXT";
+            this.EXCEL_TEXT.PasswordChar = '\0';
+            this.EXCEL_TEXT.SelectedText = "";
+            this.EXCEL_TEXT.SelectionLength = 0;
+            this.EXCEL_TEXT.SelectionStart = 0;
+            this.EXCEL_TEXT.Size = new System.Drawing.Size(614, 23);
+            this.EXCEL_TEXT.TabIndex = 3;
+            this.EXCEL_TEXT.UseSystemPasswordChar = false;
+            // 
+            // EXCEL_LABLE
+            // 
+            this.EXCEL_LABLE.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.EXCEL_LABLE.AutoSize = true;
+            this.EXCEL_LABLE.Depth = 0;
+            this.EXCEL_LABLE.Font = new System.Drawing.Font("Roboto", 11F);
+            this.EXCEL_LABLE.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.EXCEL_LABLE.Location = new System.Drawing.Point(64, 154);
+            this.EXCEL_LABLE.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.EXCEL_LABLE.MouseState = MaterialSkin.MouseState.HOVER;
+            this.EXCEL_LABLE.Name = "EXCEL_LABLE";
+            this.EXCEL_LABLE.Size = new System.Drawing.Size(206, 19);
+            this.EXCEL_LABLE.TabIndex = 2;
+            this.EXCEL_LABLE.Text = "EXCEL SAVEFULLFILENAME:";
+            // 
+            // EXPORT_BUTTON
+            // 
+            this.EXPORT_BUTTON.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.EXPORT_BUTTON.Depth = 0;
+            this.EXPORT_BUTTON.Location = new System.Drawing.Point(582, 248);
+            this.EXPORT_BUTTON.MouseState = MaterialSkin.MouseState.HOVER;
+            this.EXPORT_BUTTON.Name = "EXPORT_BUTTON";
+            this.EXPORT_BUTTON.Primary = true;
+            this.EXPORT_BUTTON.Size = new System.Drawing.Size(100, 23);
+            this.EXPORT_BUTTON.TabIndex = 4;
+            this.EXPORT_BUTTON.Text = "EXPORT";
+            this.EXPORT_BUTTON.UseVisualStyleBackColor = true;
+            this.EXPORT_BUTTON.Click += new System.EventHandler(this.EXPORT_BUTTON_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.DB_TEXT);
+            this.panel1.Controls.Add(this.ERROR_LABLE);
+            this.panel1.Controls.Add(this.DB_LABLE);
+            this.panel1.Controls.Add(this.EXPORT_BUTTON);
+            this.panel1.Controls.Add(this.EXCEL_LABLE);
+            this.panel1.Controls.Add(this.EXCEL_TEXT);
+            this.panel1.Location = new System.Drawing.Point(12, 73);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 365);
+            this.panel1.TabIndex = 5;
+            // 
+            // ERROR_LABLE
+            // 
+            this.ERROR_LABLE.AutoSize = true;
+            this.ERROR_LABLE.ForeColor = System.Drawing.Color.Red;
+            this.ERROR_LABLE.Location = new System.Drawing.Point(69, 251);
+            this.ERROR_LABLE.Name = "ERROR_LABLE";
+            this.ERROR_LABLE.Size = new System.Drawing.Size(0, 16);
+            this.ERROR_LABLE.TabIndex = 5;
+            // 
+            // DB_TEXT
+            // 
+            this.DB_TEXT.FormattingEnabled = true;
+            this.DB_TEXT.Location = new System.Drawing.Point(68, 117);
+            this.DB_TEXT.Name = "DB_TEXT";
+            this.DB_TEXT.Size = new System.Drawing.Size(614, 24);
+            this.DB_TEXT.TabIndex = 6;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "MainWindow";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MSDBTOEXCEL";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialLabel DB_LABLE;
+        private MaterialSkin.Controls.MaterialSingleLineTextField EXCEL_TEXT;
+        private MaterialSkin.Controls.MaterialLabel EXCEL_LABLE;
+        private MaterialSkin.Controls.MaterialRaisedButton EXPORT_BUTTON;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label ERROR_LABLE;
+        private System.Windows.Forms.ComboBox DB_TEXT;
+    }
+}
+
