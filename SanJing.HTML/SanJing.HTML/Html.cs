351da032afe91ef2b7559b5574c49bd0ec5d576c
37www <sanjing@inmacro.cn>
2020-01-04 01:42:48 +0800

    html
    html

--- SanJing.HTML/SanJing.HTML/Html.cs
+++ SanJing.HTML/SanJing.HTML/Html.cs
@@ -11,41 +11,94 @@ namespace SanJing.HTML
     /// <summary>
     /// HTML解析
     /// </summary>
-    public class Html
+    public static class Html
     {
         /// <summary>
-        /// 根据标签名获取节点
+        /// 获取所有标签
         /// </summary>
         /// <param name="htmlCode">HTML代码</param>
-        /// <param name="tagName">标签名</param>
         /// <returns></returns>
-        public static IHtmlNode[] GetHtmlNodeByTag(string htmlCode, string tagName)
+        public static HtmlTag[] GetHtmlTags(string htmlCode)
         {
             HtmlParser parser = new HtmlTextParser(htmlCode);
-            List<IHtmlNode> result = new List<IHtmlNode>();
+            List<HtmlTag> result = new List<HtmlTag>();
             while (parser.Traverse())
             {
                 IHtmlNode htmlNode = parser.Current;
-                GetHtmlNodeByTag(htmlNode, tagName, result);
+                GetHtmlTags(htmlNode, result);
             }
-            return result.ToArray(); ;
+            return result.ToArray();
         }
+
+        /// <summary>
+        /// 获取指定标签
+        /// </summary>
+        /// <param name="htmlCode">HTML代码</param>
+        /// <param name="tagName">标签名（小写）</param>
+        /// <returns></returns>
+        public static HtmlTag[] GetHtmlTags(string htmlCode, string tagName)
+        {
+            return GetHtmlTags(htmlCode).Where(q => q.Name == tagName).ToArray();
+        }
+
+        /// <summary>
+        /// 通过id获取标签
+        /// </summary>
+        /// <param name="htmlCode">HTML代码</param>
+        /// <param name="id">区分大小写</param>
+        /// <returns></returns>
+        public static HtmlTag GetHtmlTagById(string htmlCode, string id)
+        {
+            return GetHtmlTags(htmlCode).Where(q => q.Attributes.ContainsKey("id") && q.Attributes["id"] == id).FirstOrDefault();
+        }
+        /// <summary>
+        /// 通过class获取标签
+        /// </summary>
+        /// <param name="htmlCode">HTML代码</param>
+        /// <param name="class">区分大小写</param>
+        /// <returns></returns>
+        public static HtmlTag[] GetHtmlTagsByClass(string htmlCode, string @class)
+        {
+            return GetHtmlTags(htmlCode).Where(q => q.Attributes.ContainsKey("class"))
+                .Where(q => q.Attributes["class"] == @class || q.Attributes["class"].StartsWith($"{@class} ") ||
+                q.Attributes["class"].EndsWith($" {@class}") || q.Attributes["class"].Contains($" {@class} "))
+                .ToArray();
+        }
+
+        /// <summary>
+        /// 获取纯文本
+        /// </summary>
+        /// <param name="htmlCode">HTML代码</param>
+        /// <returns></returns>
+        public static string GetHtmlText(string htmlCode)
+        {
+            return string.Join(string.Empty, GetHtmlTags(htmlCode).Where(q => q.IsText));
+        }
+
+        /// <summary>
+        /// 获取指定标签的指定属性值
+        /// </summary>
+        /// <param name="htmlCode">HTML代码</param>
+        /// <param name="attrName">属性名（小写）</param>
+        /// <param name="tagName">标签名（小写）</param>
+        /// <returns></returns>
+        public static string[] GetAttrValues(string htmlCode, string attrName, string tagName)
+        {
+            return GetHtmlTags(htmlCode, tagName).Where(q => q.Attributes.ContainsKey(attrName))
+                .Select(q => q.Attributes[attrName]).ToArray();
+        }
+
         /// <summary>
         /// 根据标签名获取节点
         /// </summary>
         /// <param name="htmlNode">节点</param>
-        /// <param name="tagName">标签名</param>
         /// <param name="result"></param>
-        private static void GetHtmlNodeByTag(IHtmlNode htmlNode, string tagName, List<IHtmlNode> result)
+        private static void GetHtmlTags(IHtmlNode htmlNode, List<HtmlTag> result)
         {
-            if (htmlNode.Tag.ToLower() == tagName.ToLower())
-            {
-                result.Add(htmlNode);
-            }
+            result.Add(new HtmlTag(htmlNode));
+
             if (htmlNode.HasChildren)
             {
-                if (htmlNode.Tag.ToLower() == "head")
-                    return;
                 if (htmlNode.Tag.ToLower() == "script")
                     return;
                 if (htmlNode.Tag.ToLower() == "style")
@@ -54,168 +107,7 @@ namespace SanJing.HTML
                     return;
                 foreach (var childrenNode in htmlNode.Children)
                 {
-                    GetHtmlNodeByTag(childrenNode, tagName, result);
-                }
-            }
-        }
-        /// <summary>
-        /// 根据属性和值获取节点
-        /// </summary>
-        /// <param name="htmlCode">HTML代码</param>
-        /// <param name="attrName">属性名</param>
-        /// <param name="attrValue">属性值|class用匹配，其他用相等</param>
-        /// <returns></returns>
-        public static IHtmlNode[] GetHtmlNodeByAttr(string htmlCode, string attrName, string attrValue)
-        {
-            HtmlParser parser = new HtmlTextParser(htmlCode);
-            List<IHtmlNode> result = new List<IHtmlNode>();
-            while (parser.Traverse())
-            {
-                IHtmlNode htmlNode = parser.Current;
-                GetHtmlNodeByAttr(htmlNode, attrName, attrValue, result);
-            }
-            return result.ToArray(); ;
-        }
-        /// <summary>
-        /// 根据属性和值获取节点
-        /// </summary>
-        /// <param name="htmlNode">节点</param>
-        /// <param name="attrName">属性名</param>
-        /// <param name="attrValue">属性值|class用匹配，其他用相等</param>
-        /// <param name="result"></param>
-        private static void GetHtmlNodeByAttr(IHtmlNode htmlNode, string attrName, string attrValue, List<IHtmlNode> result)
-        {
-            if (htmlNode.Attributes.Where(q => !string.IsNullOrEmpty(q.Key)).Any(q => q.Key.ToLower() == attrName.ToLower() &&
-            (q.Value == attrValue ||
-            attrName.ToLower() == "class" ? ((q.Value ?? string.Empty).StartsWith(attrValue + " ") || //class用匹配，其他用相等
-            (q.Value ?? string.Empty).Contains(" " + attrValue + " ") ||
-            (q.Value ?? string.Empty).EndsWith(" " + attrValue)) : false)))
-            {
-                result.Add(htmlNode);
-            }
-            else if (htmlNode.HasChildren)
-            {
-                if (htmlNode.Tag.ToLower() == "head")
-                    return;
-                if (htmlNode.Tag.ToLower() == "script")
-                    return;
-                if (htmlNode.Tag.ToLower() == "style")
-                    return;
-                if (htmlNode.Tag.ToLower() == "iframe")
-                    return;
-                foreach (var childrenNode in htmlNode.Children)
-                {
-                    GetHtmlNodeByAttr(childrenNode, attrName, attrValue, result);
-                }
-            }
-        }
-
-        /// <summary>
-        /// 获取HTML中所有文本
-        /// </summary>
-        /// <param name="htmlCode">HTML代码</param>
-        /// <returns></returns>
-        public static string GetText(string htmlCode)
-        {
-            HtmlParser parser = new HtmlTextParser(htmlCode);
-
-            var res = new StringBuilder();
-            while (parser.Traverse())
-            {
-                IHtmlNode htmlNode = parser.Current;
-                GetText(htmlNode, res);
-            }
-            return res.ToString();
-        }
-        /// <summary>
-        /// 获取节点中所有文本
-        /// </summary>
-        /// <param name="htmlNode">节点</param>
-        /// <param name="result"></param>
-        public static void GetText(IHtmlNode htmlNode, StringBuilder result)
-        {
-            if (htmlNode.IsText)
-            {
-                if (string.IsNullOrWhiteSpace(htmlNode.Html))
-                    return;
-
-                result.Append(HttpUtility.HtmlDecode(htmlNode.Html.Trim()));
-            }
-            else if (htmlNode.HasChildren)
-            {
-                if (htmlNode.Tag.ToLower() == "head")
-                    return;
-                if (htmlNode.Tag.ToLower() == "script")
-                    return;
-                if (htmlNode.Tag.ToLower() == "style")
-                    return;
-                if (htmlNode.Tag.ToLower() == "iframe")
-                    return;
-                foreach (var childrenNode in htmlNode.Children)
-                {
-                    GetText(childrenNode, result);
-                }
-            }
-        }
-
-        /// <summary>
-        /// 获取HTML中指定标签名的指定属性
-        /// </summary>
-        /// <param name="htmlCode">HTML代码</param>
-        /// <param name="tagName">标签名</param>
-        /// <param name="attrName">属性名</param>
-        /// <returns></returns>
-        public static string[] GetTagAttr(string htmlCode, string tagName, string attrName)
-        {
-            HtmlParser parser = new HtmlTextParser(htmlCode);
-
-            var res = new List<string>();
-            while (parser.Traverse())
-            {
-                IHtmlNode htmlNode = parser.Current;
-                GetTagAttr(htmlNode, tagName, attrName, res);
-            }
-            return res.ToArray();
-        }
-        /// <summary>
-        /// 获取节点中指定标签名的指定属性
-        /// </summary>
-        /// <param name="htmlNode">节点</param>
-        /// <param name="tagName">标签名</param>
-        /// <param name="attrName">属性名</param>
-        /// <param name="result"></param>
-        public static void GetTagAttr(IHtmlNode htmlNode, string tagName, string attrName, List<string> result)
-        {
-            if (htmlNode.IsText) return;
-
-            if (htmlNode.Tag.ToLower() == tagName.ToLower())
-            {
-                if (htmlNode.Attributes != null && htmlNode.Attributes.Count > 0)
-                {
-                    foreach (var attr in htmlNode.Attributes)
-                    {
-                        if (string.IsNullOrEmpty(attr.Key)) continue;
-
-                        if (attr.Key.ToLower() == attrName.ToLower())
-                        {
-                            result.Add(attr.Value ?? string.Empty);
-                        }
-                    }
-                }
-            }
-            else if (htmlNode.HasChildren)
-            {
-                if (htmlNode.Tag.ToLower() == "head")
-                    return;
-                if (htmlNode.Tag.ToLower() == "script")
-                    return;
-                if (htmlNode.Tag.ToLower() == "style")
-                    return;
-                if (htmlNode.Tag.ToLower() == "iframe")
-                    return;
-                foreach (var childrenNode in htmlNode.Children)
-                {
-                    GetTagAttr(childrenNode, tagName, attrName, result);
+                    GetHtmlTags(childrenNode, result);
                 }
             }
         }
---
+++ SanJing.HTML/SanJing.HTML/HtmlNode.cs
@@ -0,0 +1,59 @@
+﻿using MariGold.HtmlParser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace SanJing.HTML
+{
+    /// <summary>
+    /// 节点
+    /// </summary>
+    public class HtmlTag
+    {
+        /// <summary>
+        /// 节点
+        /// </summary>
+        private IHtmlNode _htmlNode { get; set; }
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        /// <param name="htmlNode"></param>
+        public HtmlTag(IHtmlNode htmlNode)
+        {
+            _htmlNode = htmlNode;
+
+            Attributes = new Dictionary<string, string>();
+            foreach (var item in htmlNode.Attributes)
+            {
+                var key = item.Key.ToLower();
+
+                if (Attributes.ContainsKey(key))
+                    continue;
+
+                Attributes.Add(key, item.Value ?? string.Empty);
+            }
+        }
+        /// <summary>
+        /// 标签名称（全部为小写）
+        /// </summary>
+        public string Name { get { return _htmlNode.Tag.ToLower(); } }
+        /// <summary>
+        /// 是否是TEXT节点
+        /// </summary>
+        public bool IsText { get { return _htmlNode.IsText; } }
+        /// <summary>
+        /// TEXT节点的TEXT内容
+        /// </summary>
+        public string InnerText { get { return IsText ? HttpUtility.HtmlDecode(_htmlNode.Html ?? string.Empty) : string.Empty; } }
+        /// <summary>
+        /// HTML节点的HTML内容
+        /// </summary>
+        public string InnerHtml { get { return IsText ? string.Empty : _htmlNode.InnerHtml; } }
+        /// <summary>
+        /// 所有属性(Key全部为小写,如果重复只取第一个)
+        /// </summary>
+        public Dictionary<string, string> Attributes { get; private set; }
+    }
+}
