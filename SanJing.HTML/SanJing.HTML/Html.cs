7b76ec6209d73c3ae71b3097eaad2bf2a362118e
37www <50452088+37www@users.noreply.github.com>
2020-02-04 12:04:17 +0800

    html
    string empty

--- SanJing.HTML/SanJing.HTML/Html.cs
+++ SanJing.HTML/SanJing.HTML/Html.cs
@@ -20,6 +20,8 @@ namespace SanJing.HTML
         /// <returns></returns>
         public static HtmlTag[] GetHtmlTags(string htmlCode)
         {
+            if (string.IsNullOrWhiteSpace(htmlCode)) return new HtmlTag[0];
+
             HtmlParser parser = new HtmlTextParser(htmlCode);
             List<HtmlTag> result = new List<HtmlTag>();
             while (parser.Traverse())
@@ -38,6 +40,8 @@ namespace SanJing.HTML
         /// <returns></returns>
         public static HtmlTag[] GetHtmlTags(string htmlCode, string tagName)
         {
+            if (string.IsNullOrWhiteSpace(htmlCode)) return new HtmlTag[0];
+
             return GetHtmlTags(htmlCode).Where(q => q.Name == tagName).ToArray();
         }
 
@@ -49,6 +53,8 @@ namespace SanJing.HTML
         /// <returns></returns>
         public static HtmlTag GetHtmlTagById(string htmlCode, string id)
         {
+            if (string.IsNullOrWhiteSpace(htmlCode)) return null;
+
             return GetHtmlTags(htmlCode).Where(q => q.Attributes.ContainsKey("id") && q.Attributes["id"] == id).FirstOrDefault();
         }
         /// <summary>
@@ -59,6 +65,8 @@ namespace SanJing.HTML
         /// <returns></returns>
         public static HtmlTag[] GetHtmlTagsByClass(string htmlCode, string @class)
         {
+            if (string.IsNullOrWhiteSpace(htmlCode)) return new HtmlTag[0];
+
             return GetHtmlTags(htmlCode).Where(q => q.Attributes.ContainsKey("class"))
                 .Where(q => q.Attributes["class"] == @class || q.Attributes["class"].StartsWith($"{@class} ") ||
                 q.Attributes["class"].EndsWith($" {@class}") || q.Attributes["class"].Contains($" {@class} "))
@@ -72,6 +80,8 @@ namespace SanJing.HTML
         /// <returns></returns>
         public static string GetHtmlText(string htmlCode)
         {
+            if (string.IsNullOrWhiteSpace(htmlCode)) return string.Empty;
+
             return string.Join(string.Empty, GetHtmlTags(htmlCode).Where(q => q.IsText).Select(q => q.InnerText));
         }
 
@@ -84,6 +94,8 @@ namespace SanJing.HTML
         /// <returns></returns>
         public static string[] GetAttrValues(string htmlCode, string attrName, string tagName)
         {
+            if (string.IsNullOrWhiteSpace(htmlCode)) return new string[0];
+
             return GetHtmlTags(htmlCode, tagName).Where(q => q.Attributes.ContainsKey(attrName))
                 .Select(q => q.Attributes[attrName]).ToArray();
         }
@@ -95,6 +107,7 @@ namespace SanJing.HTML
         /// <param name="result"></param>
         private static void GetHtmlTags(IHtmlNode htmlNode, List<HtmlTag> result)
         {
+            
             result.Add(new HtmlTag(htmlNode));
 
             if (htmlNode.HasChildren)
