dd01014d71187cd4bb62c545c13e5b66cb782647
37www <50452088+37www@users.noreply.github.com>
2020-03-17 15:14:24 +0800

    phone
    phone

---
+++ SanJing.SMS/SanJing.SMS/Phone.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace SanJing.SMS
+{
+    /// <summary>
+    /// 手机号
+    /// </summary>
+    public class Phone
+    {
+        /// <summary>
+        /// 手机号验证【https://tcc.taobao.com/cc/json/mobile_tel_segment.htm?tel=】
+        /// </summary>
+        /// <param name="phone"></param>
+        /// <returns></returns>
+        public static bool IsValid(string phone)
+        {
+            using (var wc = new WebClient())
+            {
+                wc.Encoding = Encoding.GetEncoding("gbk");
+
+                var res = wc.DownloadString("https://tcc.taobao.com/cc/json/mobile_tel_segment.htm?tel=" + phone);
+
+                return res.Contains(phone);
+            }
+        }
+    }
+}
